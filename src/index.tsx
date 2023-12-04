4f66c5a6559d91dec93901ae1b1f929f20bcb327
Hans-Christian Otto <c.otto@suora.com>
2023-12-04 12:14:08 +0100

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
 import ReactDOM from "react-dom";
 import App from "./App.tsx";
 
-ReactDOM.render(
-  <>
-    <h1>Hallo Köln</h1>
-    <App />
-  </>,
-  document.getElementById("root"),
-);
+ReactDOM.render(<App />, document.getElementById("root"));
