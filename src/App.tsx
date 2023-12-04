e945d02ccdc26219496f9d6b21e015f1268b21ab
Hans-Christian Otto <c.otto@suora.com>
2023-12-04 14:32:47 +0100

    create TimeEntryView component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import TimeEntryView from "./components/TimeEntryView.tsx";
+
 function App() {
-  return <h1>Nennt mich gerne Christian</h1>;
+  return (
+    <div>
+      <h1>Nennt mich gerne Christian</h1>
+      <TimeEntryView comment="Hallo Welt" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TimeEntryView.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  comment: string;
+}
+
+export default function TimeEntryView(props: Props) {
+  return <div>{props.comment}</div>;
+}
