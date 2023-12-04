aec6d781fc022179f54bf232d267f8b22f2bcb89
Hans-Christian Otto <c.otto@suora.com>
2023-12-04 12:13:31 +0100

    show fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom";
 import App from "./App.tsx";
 
-const element = <App />;
+const element = (
+  <>
+    <h1>Hallo Köln</h1>
+    <App />
+  </>
+);
+
 ReactDOM.render(element, document.getElementById("root"));
