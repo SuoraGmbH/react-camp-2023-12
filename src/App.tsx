cb46d2669fa028041c32d171196dbe443c7ac58d
Hans-Christian Otto <c.otto@suora.com>
2023-12-04 16:16:57 +0100

    remove non-recommended code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import TimeEntryView, { TimeEntryView2 } from "./components/TimeEntryView.tsx";
+import TimeEntryView from "./components/TimeEntryView.tsx";
 
 function App() {
-  const timeEntryFromBackend = {
-    id: "nsiadijoasdasokdasok",
-    comment: "Hallo Welt",
-    start: new Date(),
-    end: new Date(),
-    highlight: true,
-  };
-
   return (
     <div>
       <h1>Nennt mich gerne Christian</h1>
@@ -20,25 +12,6 @@ function App() {
           end: new Date(),
         }}
       />
-
-      <TimeEntryView2
-        id="jnds"
-        comment="test"
-        start={new Date()}
-        end={new Date()}
-      />
-
-      <TimeEntryView timeEntry={timeEntryFromBackend} highlight={true} />
-
-      <TimeEntryView2
-        id={timeEntryFromBackend.id}
-        comment={timeEntryFromBackend.comment}
-        start={timeEntryFromBackend.start}
-        end={timeEntryFromBackend.end}
-        highlight={true}
-      />
-
-      <TimeEntryView2 {...timeEntryFromBackend} />
     </div>
   );
 }
--- src/components/TimeEntryView.tsx
+++ src/components/TimeEntryView.tsx
@@ -8,8 +8,4 @@ const TimeEntryView = ({ timeEntry }: Props) => {
   return <div>{timeEntry.comment}</div>;
 };
 
-export const TimeEntryView2 = (timeEntry: TimeEntry) => {
-  return <div>{timeEntry.comment}</div>;
-};
-
 export default TimeEntryView;
