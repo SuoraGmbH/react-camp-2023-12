ba4385376203db0fd8e6af1ebab519f60c4e5ab5
Hans-Christian Otto <c.otto@suora.com>
2023-12-06 14:15:22 +0100

    Add react-query provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Counter from "./components/Counter.tsx";
 import DynamicTimeEntryList from "./components/DynamicTimeEntryList.tsx";
 import GithubRepoStats from "./components/GithubRepoStats.tsx";
 import TimeEntryListFromBackend from "./components/TimeEntryListFromBackend.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 function App() {
   return (
-    <div>
       <TimeEntryListFromBackend />
       <GithubRepoStats />
+    <QueryClientProvider client={client}>
       <DynamicTimeEntryList />
       <Counter />
 
       <WiederholungsKomponente name="Günther" />
       <WiederholungsKomponente name="André" />
-    </div>
+    </QueryClientProvider>
   );
 }
 
