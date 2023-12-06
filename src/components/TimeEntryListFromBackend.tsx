5144684864ea9c2415e4cbc0660cb66792adb20f
Hans-Christian Otto <c.otto@suora.com>
2023-12-06 13:38:08 +0100

    convert time entry from backend

--- src/components/TimeEntryListFromBackend.tsx
+++ src/components/TimeEntryListFromBackend.tsx
@@ -8,13 +8,24 @@ const TimeEntryListFromBackend = () => {
   useEffect(() => {
     fetch("http://localhost:3001/timeEntries")
       .then((response) => response.json())
-      .then((data) => setTimeEntries(data));
+      .then((data) => {
+        console.log(data);
+        setTimeEntries(
+          data.map((timeEntryResponse: any) => {
+            return {
+              id: timeEntryResponse.id,
+              comment: timeEntryResponse.comment,
+              start: new Date(timeEntryResponse.start),
+              end: new Date(timeEntryResponse.end),
+            };
+          }),
+        );
+      });
   }, []);
 
   if (!timeEntries) {
     return <div>Loading...</div>;
   }
-
   return <TimeEntryList timeEntries={timeEntries} />;
 };
 
