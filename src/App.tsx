02891ad82640dd16074ccee55f4d8788d315db9a
Hans-Christian Otto <c.otto@suora.com>
2023-12-06 16:27:01 +0100

    allow MSW in browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import WiederholungsKomponente from "./components/WiederholungsKomponente.tsx";
 import Counter from "./components/Counter.tsx";
 import DynamicTimeEntryList from "./components/DynamicTimeEntryList.tsx";
-import GithubRepoStats from "./components/GithubRepoStats.tsx";
-import TimeEntryListFromBackend from "./components/TimeEntryListFromBackend.tsx";
 import GithubRepoStatsQuery from "./components/GithubRepoStatsQuery.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import GithubRepoStatsWithCustomHook from "./components/GithubRepoStatsWithCustomHook.tsx";
+import TimeEntryListFromBackend from "./components/TimeEntryListFromBackend.tsx";
 
 const client = new QueryClient({
   defaultOptions: {
@@ -18,16 +18,19 @@ function App() {
   return (
     <QueryClientProvider client={client}>
       <GithubRepoStatsQuery repoName="facebook/react" />
+      <GithubRepoStatsWithCustomHook />
+      <TimeEntryListFromBackend />
       {/*<GithubRepoStatsQuery repoName="facebook/react" />*/}
       {/*<GithubRepoStatsQuery repoName="angular/angular" />*/}
       {/*<GithubRepoStatsQuery repoName="SuoraGmbH/react-camp-2023-12" />*/}
       {/*<TimeEntryListFromBackend />*/}
       {/*<GithubRepoStats />*/}
-      <DynamicTimeEntryList />
-      <Counter />
+      {/*<DynamicTimeEntryList />*/}
 
-      <WiederholungsKomponente name="Günther" />
-      <WiederholungsKomponente name="André" />
+      {/*<Counter />*/}
+
+      {/*<WiederholungsKomponente name="Günther" />*/}
+      {/*<WiederholungsKomponente name="André" />*/}
     </QueryClientProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,23 @@
 import ReactDOM from "react-dom";
 import App from "./App.tsx";
+import { setupWorker } from "msw/browser";
+import { githubApiReposFacebookReactSuccessMock } from "./mock/api.github.com/repos/facebook/react/success.ts";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+async function enableMocking() {
+  // Nur in development wollen wir mocks haben
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  // das sollte man eigentlich etwas anders machen,
+  // damit nicht im production-bundle MSW & die mocks landen
+  // Seht dafür auf https://mswjs.io/docs/integrations/browser
+  const worker = setupWorker(githubApiReposFacebookReactSuccessMock);
+  return worker.start({ onUnhandledRequest: "bypass" });
+}
+
+// Das ist nur für development sinnvoll
+
+enableMocking().then(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
